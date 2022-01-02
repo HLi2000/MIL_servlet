4c2273a6e3a3ae4818f4cad64f3f28c091312f09
“HLi2000” <lihao20001205@outlook.com>
2022-01-02 10:42:57 +0000

    UT

--- src/main/java/DBConn.java
+++ src/main/java/DBConn.java
@@ -8,7 +8,10 @@ import java.sql.SQLException;
 
 public class DBConn {
     //set up URL and DB driver
-    private static final String dbUrl = System.getenv("JDBC_DATABASE_URL");
+    private static String USER = "gpshqobtqpqlne";
+    private static String PASSWORD = "66346f36dbd47a5aadaded42e02637792bc85123b87cbaeca609af48b5a8d700";
+    private static String DB_URL = "jdbc:postgresql://ec2-54-171-25-232.eu-west-1.compute.amazonaws.com:5432/d3lvpbc9i53ofu";
+    //private static final String dbUrl = System.getenv("JDBC_DATABASE_URL");
     private static final String DB_DRIVER = "org.postgresql.Driver";
     private static Connection conn = null;
 
@@ -21,7 +24,8 @@ public class DBConn {
 
         try {
             Class.forName(DB_DRIVER);
-            conn = DriverManager.getConnection(dbUrl);
+            conn = DriverManager.getConnection(DB_URL, USER, PASSWORD);
+            //conn = DriverManager.getConnection(dbUrl);
 
         } catch (Exception e) {
             e.printStackTrace();
